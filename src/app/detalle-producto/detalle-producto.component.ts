c61834c513f115903418856f92e0294c7c9266d8
MariLizt <lizzt005@gmail.com>
2023-05-23 15:04:59 -0500

    Subida y ajustes de contenido

--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -64,7 +64,7 @@ export class DetalleProductoComponent {
       descripcion: "Ropa casual con material suave, gran idea como un pequeño regalo de cumpleaños para mujeres, regalo para la persona que amas, transpirable. No daña la piel,un gran accesorio para mantener el calor en climas fríos y fríos, adecuado para uso casual, playa, fiesta, trabajo, etc. Uso en diferentes ocasiones.",
       precio: '$113.000',
       imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta1.jpg',
-      colores: ["negro", "rojo", "verde"]
+      colores: ["negro", "rojo", "verde","azul"]
     },{
       codigo: 2,
       id: '2',
@@ -76,16 +76,86 @@ export class DetalleProductoComponent {
     },{
       codigo: 3,
       id: '3',
-      title: "Chaqueta de Invierno",
-      descripcion: "Abrigo con capucha de felpa para mujer, chaquetas gruesas cálidas con estampado Floral, abrigo forrado de piel, chaqueta con cremallera para mujer, Invierno",
-      precio: '$100.000',
-      colores: ["negro", "azul"]
+      title: "Chaqueta de Cuero Sintético",
+      descripcion: "Hermosa chaqueta en cuerotex para mujer, ideal para cualquier ocasión, cuenta con dos bolsillos laterales funcionales, material resistente y duradero más grueso que la cuerina, esta forrada en flecce de alto calibre que protege de las bajas temperaturas.",
+      precio: '$98.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta3-negro.jpg',
+      colores: ["negro"]
+    },{
+      codigo: 4,
+      id: '4',
+      title: "Chaqueta deportiva",
+      descripcion: "Esta capa de la manera Cuenta con choque de color y ropa deportiva, manga completa y delgada. Elegancia y simplicidad es el Se centra en el diseño, es una prenda casual salvaje artículos individuales. Tejido ligero y no elástico. Silueta holgada.",
+      precio: '$58.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta4-negro.jpg',
+      colores: ["negro","azul","rosado"]
+    },{
+      codigo: 5,
+      id: '5',
+      title: "Chaqueta de Jenas",
+      descripcion: "Chaqueta rasgada, mangas largas, cierre de botón frontal, dos bolsillos en la parte delantera, chaquetas de mezclilla Tredny Fashion.",
+      precio: '$58.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta4-negro.jpg',
+      colores: ["negro","azul","gris"]
+    },{
+      codigo: 6,
+      id: '6',
+      title: "Abrigo de forro polar sintético",
+      descripcion: "Está hecho de material de alta calidad. Es lo suficientemente resistente para su uso diario. Ideal para pantalones cortos o leggings. Material suave y ligero que le proporciona una sensación cómoda de calor femenino y un aspecto elegante.",
+      precio: '$150.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta4-negro.jpg',
+      colores: ["negro","blanco","cafe"]
+    },{
+      codigo: 7,
+      id: '7',
+      title: "Pantalones deportivos",
+      descripcion: "cintura alta elástica, pantalones de yoga para entrenamiento, pantalones deportivos con bolsillos. Pantalones deportivos de ajuste holgado. Cómodo de llevar en verano, primavera u otoño. Estos pantalones pueden mostrar muy bien la curva de tus piernas largas.",
+      precio: '$40.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/pantalones1-negro.jpg',
+      colores: ["negro","beige","rosado","gris"]
+    },{
+      codigo: 8,
+      id: '8',
+      title: "Pantalón cargo",
+      descripcion: "Elaborado en algodón con costuras en contraste y corte de tiro alto. Un diseño de corte ancho y bota recta con tirantes ajustables y bolsillos laterales con broches.",
+      precio: '$160.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/pantalones8-negro.jpg',
+      colores: ["negro","verde","rosado","purpura"]
+    },{
+      codigo: 9,
+      id: '9',
+      title: "JEAN TIRO ALTO",
+      descripcion: "Nueva moda negro jeans de mujer con agujero vaqueros para mujer pantalones vaqueros mujer pantalones de jean de denim rayado pantalones femeninos",
+      precio: '$90.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/producto9-negro.jpg',
+      colores: ["negro"]
+    },{
+      codigo: 10,
+      id: '10',
+      title: "Zapatos de plataforma",
+      descripcion: "Zapatos de plataforma con aumento de altura para mujer, zapatillas de deporte para caminar, zapatos de cuña oscilantes, transpirables, ST42",
+      precio: '$110.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/producto10-negro.jpg',
+      colores: ["negro", "rosado", "azul"]
+    },{
+      codigo: 11,
+      id: '11',
+      title: "botín Forever 21",
+      descripcion: "Luce increíble junto a este botín Forever 21, su diseño con elástico lateral, aplicación de estoperoles y cómodo tacón robusto te encantará.",
+      precio: '$90.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/producto11-negro.jpg',
+      colores: ["negro","blanco","beige"]
+    },{
+      codigo: 12,
+      id: '12',
+      title: "Zapatilla Deportiva Revolution 6 NN",
+      descripcion: "Sabemos que la comodidad es la clave de una carrera exitosa, así que nos aseguramos de que tus pasos sean amortiguados y flexibles para brindar una pisada suave. Es la evolución de un favorito, con un diseño transpirable confeccionado con al menos un 20% de contenido reciclado por peso.",
+      precio: '$250.000',
+      imagen: 'http://localhost:4200/assets/img/mujer/detalle-de-producto/producto12-negro.jpg',
+      colores: ["negro","rosado"]
     }];
 
-    /*Producto1*/
-    titlep1 = 'Lorem ipsum dolor sit';
-    descripcionp1 = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, fugiat. A enim tempore, unde voluptatibus quia provident quas voluptatem vel itaque ea. Aperiam, deleniti autem!';
-    preciop1 = 120.000
+    
 }
 
 function  mensaje(){
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ProductosComponent {
 
+  listaprodArray(arr: any[], chunkSize: number): any[][] {
+    const listadoP = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      listadoP.push(arr.slice(i, i + chunkSize));
+    }
+    return listadoP;
+  }
+
   cardproductos = [{
     codigo: 1,
     title: "Chaqueta Térmica Con Capucha",
@@ -14,12 +22,49 @@ export class ProductosComponent {
   },{
     codigo: 2,
     title: "Chaqueta de Invierno",
-    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta2-negro.jpg"
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto2-negro.jpg"
   },{
     codigo: 3,
-    title: "Chaqueta de Invierno",
-    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/chaqueta2-negro.jpg"
+    title: "Chaqueta de Cuero Sintético",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto3-negro.jpg"
+  },{
+    codigo: 4,
+    title: "Chaqueta deportiva",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto4-negro.jpg"
+  },{
+    codigo: 5,
+    title: "Chaqueta de jeans",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto5-negro.jpg"
+  },{
+    codigo: 6,
+    title: "Chaqueta de jeans",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto6-negro.jpg"
+  },{
+    codigo: 7,
+    title: "Pantalón cargo",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto7-negro.jpg"
+  },{
+    codigo: 8,
+    title: "Pantalones deportivos",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto8-negro.jpg"
+  },{
+    codigo: 9,
+    title: "Pantalones deportivos",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto9-negro.jpg"
+  },{
+    codigo: 10,
+    title: "Zapatos de plataforma",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto10.jpg"
+  },{
+    codigo: 11,
+    title: "botín Forever 21",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto11.jpg"
+  },{
+    codigo: 12,
+    title: "Zapatilla Deportiva Revolution 6 NN",
+    imagen: "http://localhost:4200/assets/img/mujer/detalle-de-producto/producto12.jpg"
   }];
+
 
   /*Producto1*/
   titlep1 = 'Lorem ipsum dolor sit';
